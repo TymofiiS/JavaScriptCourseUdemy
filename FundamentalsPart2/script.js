eab04213199c9f22b788585e47b6a08ee76b44de
Tymofii Serhiienko <tymofii.serhiienko@gmail.com>
2023-09-26 22:56:11 +0300

    continue learning

--- FundamentalsPart2/script.js
+++ FundamentalsPart2/script.js
@@ -4,7 +4,59 @@
 const a = 5;
 console.log(a);
 
+// Functions
 function f1(a) { return a - 2; }
 const f2 = function (a) { return a - 2; }
 const f3 = a => a - 2;
 console.log(f1(10), f2(10), f3(10));
+
+// Add array
+const arr = [1, 2, 3];
+console.log(arr);
+const len = arr.push(4);
+console.log(arr);
+console.log(len);
+arr.unshift(0);
+console.log(arr);
+
+// Remove array
+const lastRemoved = arr.pop();
+console.log(lastRemoved);
+console.log(arr);
+const firstRemoved = arr.shift();
+console.log(firstRemoved);
+console.log(arr);
+
+// Read array
+console.log(arr.indexOf(2));
+console.log(arr.indexOf(5));
+console.log(arr.includes(2));
+console.log(arr.includes(5));
+
+const arrData = [
+    'Jon',
+    45,
+    ['Bob', 'Tim', 'Nic']
+];
+console.log(typeof arrData);
+
+// Object
+const objData = {
+    name: 'Jon',
+    age: 15,
+    friends: ['Bob', 'Tim', 'Nic'],
+
+    isFullAge: () => this.age > 18
+}
+console.log(typeof objData);
+
+console.log(objData);
+console.log(objData.name);
+console.log(objData['name']);
+
+console.log(`
+${objData.name} has ${objData.friends.length} friends.
+The best is ${objData.friends[0]}. `);
+console.log(`
+${objData.name} is ${objData.isFullAge() ? '' : 'not'} full age.`)
+
---
+++ IdeSetupPart3/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const abcd = 'abcd';
+console.log(abcd);
+console.log('test message');
+
+console.log('third message');
+
+// input [17, 21, 23]
+// will print
+// "... 17C in 1 days ... 21C in 2 days ... 23C in 3 days"
