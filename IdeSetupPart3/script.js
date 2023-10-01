8c26923cc00421186a8f7c5041251bb553d73ecf
Tymofii Serhiienko <tymofii.serhiienko@gmail.com>
2023-10-01 17:48:16 +0300

    continue learning

--- IdeSetupPart3/script.js
+++ IdeSetupPart3/script.js
@@ -9,3 +9,9 @@ console.log('third message');
 // input [17, 21, 23]
 // will print
 // "... 17C in 1 days ... 21C in 2 days ... 23C in 3 days"
+const input = [17, 21, 23];
+let res = '';
+let day = 1;
+input.forEach(x => (res += `... ${x}C in ${day++} days`));
+console.log(res);
+console.log('yes + 1');
